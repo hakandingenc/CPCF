29a4abe4b0fdeb644dcd6de6a06c5a783489ff33
Hakan Dingenc <hakan.dingenc@u.northwestern.edu>
2019-09-12 14:45:43 -0500

    Begin reformulating complete monitoring

--- tests/complete-monitoring.rkt
+++ tests/complete-monitoring.rkt
@@ -4,7 +4,9 @@
          "../src/languages.rkt"
          "../src/reduction-semantics.rkt"
          "../src/well-formedness.rkt"
-         "../src/utils.rkt")
+         "../src/typing-judgment.rkt"
+         "../src/utils.rkt"
+         "../src/translation.rkt")
 
 (define-union-language CPCF-O-Γ-Δ
   CPCF-O-Γ CPCF-O-Δ)
@@ -21,21 +23,20 @@
 (define (typecheck e)
   (judgment-holds (types ∘ e t) t))
 
-(define match-value (redex-match? CPCF-O v))
+(define match-value (redex-match? CPCF-IO v))
 
 ; This formulation of the theorem is technically stronger
 ; (i.e. has stronger assumptions) because it checks for nondeterministic reduction
 (define n 0)
 #;
 (redex-check
- CPCF-O-Γ-Δ-core
- core-e
- #:in-order
- (if (judgment-holds (suitable core-e))
+ CPCF-O-Γ
+ e
+ (if (judgment-holds (suitable e))
      (let ([reduction
             (apply-reduction-relation*
-             CPCF-O-red
-             (term core-e))])
+             CPCF-IO-red
+             (term e))])
        (or
         (and (= (length reduction) 1)
              (let ([irreducible (first reduction)])
@@ -43,39 +44,54 @@
                 ;; reduces to a value
                 (match-value irreducible)
                 ;; diverges (note: not explicitly checking ∀e)
-                (= (length (apply-reduction-relation CPCF-O-red irreducible)) 1))))
+                (= (length (apply-reduction-relation CPCF-IO-red irreducible)) 1))))
         ;; correctly errors
         #;
         (let ([all-reductions (apply-reduction-relation*
-                               CPCF-O-red
-                               (term core-e)
+                               CPCF-IO-red
+                               (term e)
                                #:all? #t)])
-          (and (redex-match CPCF-O
+          (and (redex-match CPCF-IO
 
                             (last all-reductions))
                ; TO-DO: Check if there's at least one such thing
                (map (redex-match
-                     CPCF-O
+                     CPCF-IO
                      (in-hole E
                               (mon (k l-mon j)
                                    (flat-ob e (l ...))
                                    v)))
                     all-reductions))
           ))
-       (println (term core-e)))
+       (println (term e)))
      (set! n (add1 n)))
  #:print? #t
  #:attempts 1000)
-n
+#;n
+#;
+(redex-check
+ CPCF-O-Γ
+ e
+ #:ad-hoc
+ (when (and
+        (judgment-holds (suitable (term e)))
+        (member 'mon (flatten (term e))))
+   (println (term e)))
+ #:attempts 1000000
+ #:keep-going? #t)
+
+(define-extended-language source*
+  source
+  [constants ∘ ∪ :])
 
 (redex-check
- CPCF-O-Γ-Δ-core
- core-e
- #:ad-hoc
- (if
-  (and
-   (judgment-holds (suitable (term core-e)))
-   (member 'mon (flatten (term core-e))))
-  (println (term core-e))
-  #t)
-  #:attempts 10000000)
+ source*
+ p
+ (let ([my-term (term (decorate "b/o" p))])
+   (when (and
+          (judgment-holds (suitable ,my-term))
+          (println (term p))
+          #;(member 'mon (flatten my-term)))
+     (println my-term)))
+ #:attempts 50000
+ #:keep-going? #t)
